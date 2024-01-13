826493e3edccfac9caeb36b095a8457a8c2d2e4f
yarou007 <yacine.hami@esen.tn>
2024-01-13 18:39:52 +0100

    the time i was out , they pull me back in

---
+++ TP1Exercice3/Article.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1Exercice3
+{
+    internal class Article
+    {
+        private string refrence;
+        private string designation;
+        private double prixHT;
+        static double tauxTVA = 0.19;
+        
+
+        public Article()
+        {
+        }
+        public Article(string refrence, string designation, double prixHT)
+        {
+            this.refrence = refrence;
+            this.designation = designation;
+            this.prixHT = prixHT;
+        }
+        public Article(string reference, string designation)
+        {
+            this.designation = designation;
+            this.refrence = reference;
+        }
+
+        Article a;
+        public Article(Article a)
+        {
+             this.a= a;
+        }
+        public double CalculerPrixTTC()
+        {
+            return prixHT + (prixHT * tauxTVA / 100);
+        }
+        public void AfficherArticle()
+        {
+            Console.WriteLine("reference : "+this.refrence+" designation : "+this.designation+" prixHT : "+this.prixHT
+                +" TVA "+tauxTVA);
+        }
+    }
+}
---
+++ TP1Exercice3/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace TP1Exercice3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string rep;
+            do
+            {
+                Console.WriteLine("Donner reference : ");
+                string reff = Console.ReadLine();
+                Console.WriteLine("Donner designation : ");
+                string des = Console.ReadLine();
+                Console.WriteLine("Donner prix : ");
+                double prix = double.Parse(Console.ReadLine());
+                Article a = new Article(reff, des, prix);
+                a.AfficherArticle();
+                Console.WriteLine( "Prix = " + a.CalculerPrixTTC());
+                Console.WriteLine("Ajouter autre article : O pour oui N pour non ");
+                rep = Console.ReadLine();
+            } while (rep.Equals("O") );
+        }
+
+    }
+}
